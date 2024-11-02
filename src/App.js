7cc5194d08d0de946ac5e2277b12e34ebf1ef3ec
Danish Shafi <danish@equitas.ai>
2024-11-02 16:51:41 +0000

    initial install

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from '@chakra-ui/react';
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to Durhack Team SlowPumaSomething</p>
+      <Button>I am a Chakra UI Button</Button>
+    </div>
+  );
+}
+
+export default App;
